1aba475f26068c481fa408460c419703259068d1
Harley Macedo <harley.ip@gmail.com>
2020-07-09 10:05:10 -0300

    Exemplo handlebars melhorado

--- handlebars/app.js
+++ handlebars/app.js
@@ -7,13 +7,18 @@ app.engine('handlebars', handleBars({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 
 app.get('/cliente', (req, res) => { 
-    res.render('clienteView', {nome: 'Ana', sobrenome: 'Carvalho', tags: tagsForm()})
-})
-
-app.listen(3000, () => {
-    console.log('App rodando!')
+    res.render('clienteView', { nome: 'Ana', sobrenome: 'Carvalho', tags: tagsForm() })
 })
 
 function tagsForm () {
     return {tag1: 'form', tag2: 'canvas', tag3: 'table'}
 }
+
+app.get('/clientes', (req, res) => {
+    let contexto = { layout: 'main', pessoas: [{nome: 'nome1', idade: 26},{nome: 'nome2', idade: 39}]}
+    res.render('clientesView',  contexto)
+})
+
+app.listen(3000, () => {
+    console.log('App rodando!')
+})
