8017dedb95e6ebf15af0632afcae03f8392e9afd
Harley Macedo <harley.ip@gmail.com>
2020-07-09 07:00:35 -0300

    Helpers do hdb implementado

--- handlebars/app.js
+++ handlebars/app.js
@@ -7,9 +7,13 @@ app.engine('handlebars', handleBars({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 
 app.get('/cliente', (req, res) => { 
-    res.render('clienteView', {nome: 'Ana', sobrenome: 'Carvalho'})
+    res.render('clienteView', {nome: 'Ana', sobrenome: 'Carvalho', tags: tagsForm()})
 })
 
 app.listen(3000, () => {
     console.log('App rodando!')
 })
+
+function tagsForm () {
+    return {tag1: 'form', tag2: 'canvas', tag3: 'table'}
+}
