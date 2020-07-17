be9796d76ced13a6e874d22b2057a2bc67e67a66
Harley Macedo <harley.ip@gmail.com>
2020-07-17 11:55:26 -0300

    Exemplos body-parser e method-override modificados

--- body-parser/index.js
+++ body-parser/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 
-app.use(bodyParser.urlencoded( {extended: false} ))
+app.use(bodyParser.urlencoded( {extended: true} ))
 
 app.get('/form', (req, res) => {
     let html = `
